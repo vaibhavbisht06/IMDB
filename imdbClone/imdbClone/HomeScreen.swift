5a31ceba0f32d4ceb1568957f48b4a1476c2c8b2
Vaibhav Bisht <vaibhavbisht06@gmail.com>
2020-09-06 21:29:32 +0530

    Initial Commit

---
+++ imdbClone/imdbClone/HomeScreen.swift
@@ -0,0 +1,13 @@
+//
+//  HomeScreen.swift
+//  imdbClone
+//
+//  Created by Vaibhav Bisht on 06/09/20.
+//  Copyright © 2020 Vaibhav Bisht. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreen : UIViewController {
+    
+}
---
+++ imdbClone/imdbClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imdbClone
+//
+//  Created by Vaibhav Bisht on 06/09/20.
+//  Copyright © 2020 Vaibhav Bisht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

317d343e4023e0eb8fcfd552191a6f736a5b8aa1
Vaibhav Bisht <vaibhavbisht06@gmail.com>
2020-09-17 21:35:33 +0530

    L

---
+++ imdbClone/imdbClone/Models/ProductModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProductModel.swift
+//  imdbClone
+//
+//  Created by Vaibhav Bisht on 13/09/20.
+//  Copyright © 2020 Vaibhav Bisht. All rights reserved.
+//
+
+import Foundation
+
+struct ProductModel: Codable {
+    let Search : search
+}
